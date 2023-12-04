802831a03744d8ffc3b093bebccbc7a150f956af
Marian <mrianhricuk@gmail.com>
2023-12-04 10:51:07 +0200

    h2

--- src/main/java/org/example/cofig/Configyration.java
+++ src/main/java/org/example/cofig/Configyration.java
@@ -5,6 +5,10 @@ import org.flywaydb.core.Flyway;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Configyration {
   private static final Configyration INSTANCE = new Configyration();
@@ -14,13 +18,28 @@ public class Configyration {
     this.sessionFactory = new Configuration()
             .addAnnotatedClass(Planet.class)
             .buildSessionFactory();
-    flywayMigration(PropertyReader.getConnectionUrlForPostgres(),
+
+    /*flywayMigration(PropertyReader.getConnectionUrlForPostgres(),
             PropertyReader.getUserForPostgres(),
-            PropertyReader.getPasswordForPostgres());
+            PropertyReader.getPasswordForPostgres());*/
+    flyli();
 
   }
+
+  static final String JDBC_DRIVER = "org.h2.Driver";
+  static final String DB_URL = "jdbc:h2:./test";
+
+  static final String USER = "sa";
+  static final String PASS = "";
   public static Configyration getInstance() {
     return INSTANCE;
+  }
+  public Connection getConections() {
+    try {
+      return DriverManager.getConnection(DB_URL, USER, PASS);
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public SessionFactory getSessionFactory() {
@@ -32,8 +51,15 @@ public class Configyration {
   }
 
 
-  private void flywayMigration(String connectionUrl, String username, String password) {
+  /*private void flywayMigration(String connectionUrl, String username, String password) {
     Flyway flyway = Flyway.configure().dataSource(connectionUrl, username, password).load();
+    flyway.migrate();
+  }*/
+  public void flyli() {
+
+
+    Flyway flyway = Flyway.configure().dataSource(DB_URL,
+            USER, PASS).load();
     flyway.migrate();
   }
 
